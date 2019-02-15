b050f354e19626af4cfa44d4e22d6c53702e320e
Henrique Cavalcante <=>
2019-02-15 03:54:05 -0200

    Site - Carrinho de Compras - parte 3 - Cálculo de Frete Correios

--- site-cart.php
+++ site-cart.php
@@ -15,7 +15,8 @@ $app->get('/cart', function() {
 	$page = new Page();
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 });
 
@@ -59,6 +60,13 @@ $app->get('/cart/:idproduct/remove', function($idproduct) {
 	$cart = Cart::getFromSession();
 	$cart->removeProduct($product, true); //TRUE => remove todos
 
+	header("Location: /cart");
+	exit;
+});
+
+$app->post("/cart/freight", function(){
+	$cart = Cart::getFromSession();
+	$cart->setFreight($_POST['zipcode']);
 	header("Location: /cart");
 	exit;
 });
